1d42eb704e6555c79212e993a92a97be1f2323f2
alpha-tango-kilo <git@heyatk.com>
2021-07-24 19:21:02 +0100

    Test clap & RuntimeConfig argument parsing
    Split up RuntimeConfig creation into 3 functions:
    1. create_clap_app (private)
    2. process_matches (private)
    3. new             (public)
    This allows for easier testing of clap's rules,
    plus the hand-done changes
    Disabled atty checks when testing

--- src/clap_wrap.rs
+++ src/clap_wrap.rs
@@ -1,6 +1,7 @@
 use crate::RunError;
+#[cfg(not(test))]
 use atty::Stream;
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 
 pub struct RuntimeConfig {
     pub search_term: String,
@@ -10,33 +11,51 @@ pub struct RuntimeConfig {
 
 impl RuntimeConfig {
     pub fn new() -> Self {
-        let clap = App::new(env!("CARGO_PKG_NAME"))
+        RuntimeConfig::process_matches(&RuntimeConfig::create_clap_app().get_matches())
+    }
+
+    #[inline]
+    fn create_clap_app() -> clap::App<'static> {
+        App::new(env!("CARGO_PKG_NAME"))
             .version(env!("CARGO_PKG_VERSION"))
             .author("alpha-tango-kilo <git@heyatk.com>")
             .about(env!("CARGO_PKG_DESCRIPTION"))
-            .arg(Arg::new("non-interactive")
-                .short('n')
-                .long("non-interactive")
-                .about("Disables interactive features (effectively makes search \"I'm feeling lucky\"). Implies -r 1"))
-            .arg(Arg::new("number_of_results")
-                .short('r')
-                .long("results")
-                .about("The maximum number of results to show from IMDb")
-                .takes_value(true)
-                .validator(|s| s.parse::<usize>().map_err(|_| RunError::ClapNotUsize)))
-            .arg(Arg::new("search_term")
-                .about("The title of the movie/show you're looking for")
-                .required(true))
-            .get_matches();
+            .arg(
+                Arg::new("non-interactive")
+                    .short('n')
+                    .long("non-interactive")
+                    .about("Disables interactive features (always picks the first result)"),
+            )
+            .arg(
+                Arg::new("number_of_results")
+                    .short('r')
+                    .long("results")
+                    .about("The maximum number of results to show from IMDb")
+                    .takes_value(true)
+                    .conflicts_with("non-interactive")
+                    .validator(|s| s.parse::<usize>().map_err(|_| RunError::ClapNotUsize)),
+            )
+            .arg(
+                Arg::new("search_term")
+                    .about("The title of the movie/show you're looking for")
+                    .required(true),
+            )
+    }
 
-        let search_term = clap.value_of("search_term").unwrap().to_string();
+    #[inline]
+    fn process_matches(clap_matches: &ArgMatches) -> Self {
+        let search_term = clap_matches.value_of("search_term").unwrap().to_string();
 
-        let interactive = !clap.is_present("non-interactive")
+        // Note: atty checks are disabled for testing
+        #[cfg(not(test))]
+        let interactive = !clap_matches.is_present("non-interactive")
             && atty::is(Stream::Stdout)
             && atty::is(Stream::Stdin);
+        #[cfg(test)]
+        let interactive = !clap_matches.is_present("non-interactive");
 
         let number_of_results = if interactive {
-            match clap.value_of("number_of_results") {
+            match clap_matches.value_of("number_of_results") {
                 Some(n) => n.parse().unwrap(),
                 None => RuntimeConfig::default().number_of_results,
             }
@@ -59,5 +78,123 @@ impl Default for RuntimeConfig {
             interactive: true,
             number_of_results: 10,
         }
+    }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::*;
+
+    #[test]
+    fn help() {
+        let clap = RuntimeConfig::create_clap_app();
+        let m = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME"), "-h"])
+            .unwrap_err();
+        assert_eq!(m.kind, clap::ErrorKind::DisplayHelp);
+
+        let clap = RuntimeConfig::create_clap_app();
+        let m = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME"), "--help"])
+            .unwrap_err();
+        assert_eq!(m.kind, clap::ErrorKind::DisplayHelp);
+    }
+
+    #[test]
+    fn version() {
+        let clap = RuntimeConfig::create_clap_app();
+        let err = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME"), "-V"])
+            .unwrap_err();
+        assert_eq!(err.kind, clap::ErrorKind::DisplayVersion);
+
+        let clap = RuntimeConfig::create_clap_app();
+        let err = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME"), "--version"])
+            .unwrap_err();
+        assert_eq!(err.kind, clap::ErrorKind::DisplayVersion);
+    }
+
+    #[test]
+    fn results_short() {
+        let clap = RuntimeConfig::create_clap_app();
+        let m = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME"), "-r", "3", "foo"])
+            .unwrap();
+        assert_eq!(m.value_of("number_of_results"), Some("3"));
+
+        let config = RuntimeConfig::process_matches(&m);
+        assert_eq!(config.number_of_results, 3);
+    }
+
+    #[test]
+    fn results_long() {
+        let clap = RuntimeConfig::create_clap_app();
+        let m = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME"), "--results", "7", "foo"])
+            .unwrap();
+        assert_eq!(m.value_of("number_of_results"), Some("7"));
+
+        let config = RuntimeConfig::process_matches(&m);
+        assert_eq!(config.number_of_results, 7);
+    }
+
+    #[test]
+    fn results_invalid() {
+        let clap = RuntimeConfig::create_clap_app();
+        let err = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME"), "--results", "bar", "foo"])
+            .unwrap_err();
+        assert_eq!(err.kind, clap::ErrorKind::ValueValidation);
+    }
+
+    #[test]
+    fn non_interactive_short() {
+        let clap = RuntimeConfig::create_clap_app();
+        let m = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME"), "-n", "foo"])
+            .unwrap();
+        assert!(m.is_present("non-interactive"));
+
+        let config = RuntimeConfig::process_matches(&m);
+        assert!(!config.interactive);
+        assert_eq!(config.number_of_results, 1);
+    }
+
+    #[test]
+    fn non_interactive_long() {
+        let clap = RuntimeConfig::create_clap_app();
+        let m = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME"), "--non-interactive", "foo"])
+            .unwrap();
+        assert!(m.is_present("non-interactive"));
+
+        let config = RuntimeConfig::process_matches(&m);
+        assert!(!config.interactive);
+        assert_eq!(config.number_of_results, 1);
+    }
+
+    #[test]
+    fn conflicting_r_n() {
+        let clap = RuntimeConfig::create_clap_app();
+        let err = clap
+            .try_get_matches_from(vec![
+                env!("CARGO_PKG_NAME"),
+                "--non-interactive",
+                "--results",
+                "5",
+                "foo",
+            ])
+            .unwrap_err();
+        assert_eq!(err.kind, clap::ErrorKind::ArgumentConflict);
+    }
+
+    #[test]
+    fn no_search_term() {
+        let clap = RuntimeConfig::create_clap_app();
+        let err = clap
+            .try_get_matches_from(vec![env!("CARGO_PKG_NAME")])
+            .unwrap_err();
+        assert_eq!(err.kind, clap::ErrorKind::MissingRequiredArgument);
     }
 }
