f462e89aaf3a0470f4742fd0b383a6c998370729
alpha-tango-kilo <git@heyatk.com>
2021-12-29 19:49:43 +0000

    Sort out theming
    Make once_cell a regular dependency (it was already in-tree)
    Use a constant for the theme (saves 2-3 batches of allocations per program run)
    Don't actually customise the theme

--- src/user_input.rs
+++ src/user_input.rs
@@ -3,12 +3,14 @@ use crate::RunError::InputUserHalted;
 use crate::{reqwest, Result};
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::{Input, Select};
+use once_cell::unsync::Lazy;
 use std::fmt::Display;
+use std::ops::Deref;
 
-// TODO: customise theme
+const THEME: Lazy<ColorfulTheme> = Lazy::new(Default::default);
 
 pub fn get_api_key(client: &reqwest::Client) -> Result<String> {
-    let api_key = Input::with_theme(&ColorfulTheme::default())
+    let api_key = Input::with_theme(THEME.deref())
         .with_prompt("Please enter in your OMDb API key. If you need to, visit their website to get one (https://www.omdbapi.com/apikey.aspx)")
         .validate_with(|api_key: &String| test_api_key(api_key, client))
         .interact_text()?;
@@ -16,7 +18,7 @@ pub fn get_api_key(client: &reqwest::Client) -> Result<String> {
 }
 
 pub fn get_search_term() -> Result<String> {
-    let question = Input::with_theme(&ColorfulTheme::default())
+    let question = Input::with_theme(THEME.deref())
         .with_prompt(
             "Please enter the name of the movie/show you're looking for",
         )
@@ -25,7 +27,7 @@ pub fn get_search_term() -> Result<String> {
 }
 
 pub fn choose_result_from<E: Display>(entries: &[E]) -> Result<&E> {
-    Select::with_theme(&ColorfulTheme::default())
+    Select::with_theme(THEME.deref())
         .with_prompt("Pick the correct search result (Esc or Q to quit)")
         .items(entries)
         .interact_opt()?
